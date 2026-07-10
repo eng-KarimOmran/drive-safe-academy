a3fa0d8c6b9341aa4e2ac7a2edd6e6f1bfecb8b5
Karim Omran <eng.karimomran@gmail.com>
2026-07-10 08:07:27 +0300

    Meta Pixel

--- app/script/FacebookPixel.tsx
+++ app/script/FacebookPixel.tsx
@@ -1,46 +1,38 @@
 import Script from "next/script";
 
-type FacebookPixelProps = {
-  pixelId?: string;
-};
-
-export default function FacebookPixel({ pixelId }: FacebookPixelProps) {
-  if (!pixelId) return;
+export default function FacebookPixel() {
   return (
     <>
-      <Script id="facebook-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;
-          n.push=n;
-          n.loaded=!0;
-          n.version='2.0';
-          n.queue=[];
-          t=b.createElement(e);
-          t.async=!0;
-          t.src=v;
-          s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s);
-          }(window, document, 'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-
-          fbq('init', '${pixelId}');
-          fbq('track', 'PageView');
-        `}
-      </Script>
-
+      {/* Meta Pixel Code */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1598851514959763');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
       <noscript>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          height={1}
-          width={1}
+          height="1"
+          width="1"
           style={{ display: "none" }}
-          src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          src="https://www.facebook.com/tr?id=1598851514959763&ev=PageView&noscript=1"
           alt=""
         />
       </noscript>
+      {/* End Meta Pixel Code */}
     </>
   );
 }
