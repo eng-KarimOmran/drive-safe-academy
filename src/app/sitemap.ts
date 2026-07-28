09162d2582f7d0fe1b2afbcf70fd614fd6e2a770
Karim Omran <eng.karimomran@gmail.com>
2026-07-28 09:56:25 +0300

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,13 +11,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 1,
         },
         {
-            url: `${baseUrl}/programs`,
+            url: `${baseUrl}/#programs`,
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 0.9,
         },
         {
-            url: `${baseUrl}/contact`,
+            url: `${baseUrl}/#contact`,
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 0.7,
